47f8bc8dbf61bfcdcb9d3caa21fe4f0177ea82bf
Tatyana <tatiana.kosenchuk@gmail.com>
2022-11-29 23:45:20 +0300

    Method

---
+++ Code/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] TextNotMore3Symbols(string[] text)
+{
+    int maxlength = 3;
+    string[] result = new string[text.Length];
+    int count = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= maxlength) result[count] = text[i];
+        count++;
+    }
+    return result;
+}
+
